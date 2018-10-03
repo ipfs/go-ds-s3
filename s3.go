fe783bfb8a6e265cbd40a60a5ca367099358cf31
Edgar Lee <edgarl@netflix.com>
2018-10-03 16:27:47 -0700

    Add functional support with go-ipfs#5516

--- s3.go
+++ s3.go
@@ -1,211 +1,309 @@
 package s3ds
 
 import (
-	"encoding/xml"
+	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net/http"
+	"path"
 	"strings"
-	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	ds "github.com/ipfs/go-datastore"
 	dsq "github.com/ipfs/go-datastore/query"
-	s3 "github.com/rlmcpherson/s3gof3r"
 )
 
-type S3Bucket struct {
-	s3c  *s3.S3
-	buck *s3.Bucket
+// listMax is the largest amount of objects you can request from S3 in a list call
+const listMax = 1000
 
-	opcfg *s3.Config
+const defaultConcurrency = 100
+
+type S3Bucket struct {
+	Config
+	S3      *s3.S3
+	limiter chan struct{}
 }
 
 type Config struct {
-	Domain    string
-	AccessKey string
-	SecretKey string
-	Bucket    string
+	AccessKey      string
+	SecretKey      string
+	SessionToken   string
+	Bucket         string
+	Region         string
+	RegionEndpoint string
+	RootDirectory  string
+	Concurrency    int
 }
 
-func NewS3Datastore(cfg *Config) *S3Bucket {
-	keys := s3.Keys{
-		AccessKey: cfg.AccessKey,
-		SecretKey: cfg.SecretKey,
+func NewS3Datastore(conf Config) (*S3Bucket, error) {
+	if conf.Concurrency == 0 {
+		conf.Concurrency = defaultConcurrency
 	}
-	c := s3.New(cfg.Domain, keys)
-	buck := c.Bucket(cfg.Bucket)
+
+	awsConfig := aws.NewConfig()
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new session: %s", err)
+	}
+
+	creds := credentials.NewChainCredentials([]credentials.Provider{
+		&credentials.StaticProvider{Value: credentials.Value{
+			AccessKeyID:     conf.AccessKey,
+			SecretAccessKey: conf.SecretKey,
+			SessionToken:    conf.SessionToken,
+		}},
+		&credentials.EnvProvider{},
+		&credentials.SharedCredentialsProvider{},
+		&ec2rolecreds.EC2RoleProvider{Client: ec2metadata.New(sess)},
+	})
+
+	if conf.RegionEndpoint != "" {
+		awsConfig.WithS3ForcePathStyle(true)
+		awsConfig.WithEndpoint(conf.RegionEndpoint)
+	}
+
+	awsConfig.WithCredentials(creds)
+	awsConfig.WithRegion(conf.Region)
+
+	sess, err = session.NewSession(awsConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new session with aws config: %s", err)
+	}
+	s3obj := s3.New(sess)
+
 	return &S3Bucket{
-		s3c:  c,
-		buck: buck,
-		opcfg: &s3.Config{
-			Concurrency: 1,
-			PartSize:    s3.DefaultConfig.PartSize,
-			NTry:        10,
-			Md5Check:    true,
-			Scheme:      "https",
-			Client:      s3.ClientWithTimeout(5 * time.Second),
-		},
-	}
+		S3:      s3obj,
+		Config:  conf,
+		limiter: make(chan struct{}, conf.Concurrency),
+	}, nil
+}
+
+func (s *S3Bucket) Put(k ds.Key, value []byte) error {
+	_, err := s.S3.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(s.s3Path(k.String())),
+		Body:   bytes.NewReader(value),
+	})
+	return parseError(err)
 }
 
 func (s *S3Bucket) Get(k ds.Key) ([]byte, error) {
-	r, _, err := s.buck.GetReader(k.String(), s.opcfg)
-	switch err := err.(type) {
-	case *s3.RespError:
-		if err.StatusCode == 404 {
-			return nil, ds.ErrNotFound
-		}
-		return nil, err
-	default:
-		return nil, err
-	case nil:
-		// continue
-	}
-	defer r.Close()
-
-	out, err := ioutil.ReadAll(r)
+	resp, err := s.S3.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(s.s3Path(k.String())),
+	})
 	if err != nil {
-		return nil, err
+		return nil, parseError(err)
 	}
+	defer resp.Body.Close()
 
-	return out, nil
+	return ioutil.ReadAll(resp.Body)
+}
+
+func (s *S3Bucket) Has(k ds.Key) (exists bool, err error) {
+	_, err = s.S3.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(s.s3Path(k.String())),
+	})
+	if err != nil {
+		if parseError(err) == ds.ErrNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
 func (s *S3Bucket) Delete(k ds.Key) error {
-	return s.buck.Delete(k.String())
-}
-
-func (s *S3Bucket) Put(k ds.Key, val []byte) error {
-	w, err := s.buck.PutWriter(k.String(), nil, s.opcfg)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
-
-	_, err = w.Write(val)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s *S3Bucket) Has(k ds.Key) (bool, error) {
-	url := fmt.Sprintf("https://%s.%s%s", s.buck.Name, s.buck.Domain, k.String())
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Set("If-Modified-Since", time.Now().Format(time.RFC1123))
-
-	s.buck.Sign(req)
-	resp, err := s.buck.Do(req)
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-	io.Copy(ioutil.Discard, resp.Body)
-
-	switch resp.StatusCode {
-	case 200, 304:
-		return true, nil
-	case 404:
-		return false, nil
-	default:
-		return false, fmt.Errorf(resp.Status)
-	}
+	_, err := s.S3.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(s.Bucket),
+		Key:    aws.String(s.s3Path(k.String())),
+	})
+	return parseError(err)
 }
 
 func (s *S3Bucket) Query(q dsq.Query) (dsq.Results, error) {
-	if !q.KeysOnly {
-		return nil, fmt.Errorf("s3 datastore doesnt support returning values in query, only keys")
-	}
-
-	if q.Prefix != "" {
-		return nil, fmt.Errorf("s3ds doesnt support prefixes")
-	}
-
 	if q.Orders != nil || q.Filters != nil {
-		return nil, fmt.Errorf("s3ds doesnt support filters or orders")
+		return nil, fmt.Errorf("s3ds doesn't support filters or orders")
 	}
 
-	req, err := http.NewRequest("GET", "https://"+s.buck.Name+"."+s.buck.Domain, nil)
+	limit := q.Limit + q.Offset
+	if limit == 0 || limit > listMax {
+		limit = listMax
+	}
+
+	resp, err := s.S3.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(s.Bucket),
+		Prefix:    aws.String(s.s3Path(q.Prefix)),
+		Delimiter: aws.String("/"),
+		MaxKeys:   aws.Int64(int64(limit)),
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	s.buck.Sign(req)
-
-	resp, err := s.buck.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	dec := xml.NewDecoder(resp.Body)
-	var nextiskey bool
-
+	index := q.Offset
 	nextValue := func() (dsq.Result, bool) {
-		for {
-			tok, err := dec.Token()
+		for index >= len(resp.Contents) {
+			if !*resp.IsTruncated {
+				return dsq.Result{}, false
+			}
+
+			resp, err = s.S3.ListObjectsV2(&s3.ListObjectsV2Input{
+				Bucket:            aws.String(s.Bucket),
+				Prefix:            aws.String(s.s3Path(q.Prefix)),
+				Delimiter:         aws.String("/"),
+				MaxKeys:           aws.Int64(listMax),
+				ContinuationToken: resp.NextContinuationToken,
+			})
 			if err != nil {
-				if err == io.EOF {
-					return dsq.Result{}, false
-				}
 				return dsq.Result{Error: err}, false
 			}
 
-			switch tok := tok.(type) {
-			case xml.StartElement:
-				if tok.Name.Local == "Key" {
-					nextiskey = true
-				}
-			case xml.CharData:
-				if nextiskey {
-					kval := string(tok)
-					nextiskey = false
-					dec.Skip()
-
-					if strings.HasPrefix(kval, ".md5/") {
-						continue
-					}
-					res := dsq.Result{Entry: dsq.Entry{Key: kval}}
-					return res, true
-				}
+			index -= len(resp.Contents)
+			if index < 0 {
+				index = 0
 			}
 		}
+
+		entry := dsq.Entry{
+			Key: *resp.Contents[index].Key,
+		}
+		if !q.KeysOnly {
+			value, err := s.Get(ds.NewKey(entry.Key))
+			if err != nil {
+				return dsq.Result{Error: err}, false
+			}
+			entry.Value = value
+		}
+
+		index++
+		return dsq.Result{Entry: entry}, true
 	}
 
 	return dsq.ResultsFromIterator(q, dsq.Iterator{
 		Close: func() error {
-			return resp.Body.Close()
+			return nil
 		},
 		Next: nextValue,
 	}), nil
+}
+
+func (s *S3Bucket) Batch() (ds.Batch, error) {
+	return &s3Batch{
+		s:   s,
+		ops: make(map[string]batchOp),
+	}, nil
 }
 
 func (s *S3Bucket) Close() error {
 	return nil
 }
 
-type s3Batch struct {
-	s *S3Bucket
+func (s *S3Bucket) s3Path(p string) string {
+	return path.Join(s.RootDirectory, p)
 }
 
-func (s *S3Bucket) Batch() (ds.Batch, error) {
-	return &s3Batch{s}, nil
+func parseError(err error) error {
+	if s3Err, ok := err.(awserr.Error); ok && s3Err.Code() == s3.ErrCodeNoSuchKey {
+		return ds.ErrNotFound
+	}
+	return err
+}
+
+type s3Batch struct {
+	s   *S3Bucket
+	ops map[string]batchOp
+}
+
+type batchOp struct {
+	val    []byte
+	delete bool
 }
 
 func (b *s3Batch) Put(k ds.Key, val []byte) error {
-	return b.s.Put(k, val)
+	b.ops[k.String()] = batchOp{
+		val:    val,
+		delete: false,
+	}
+	return nil
 }
 
 func (b *s3Batch) Delete(k ds.Key) error {
-	return b.s.Delete(k)
+	b.ops[k.String()] = batchOp{
+		val:    nil,
+		delete: true,
+	}
+	return nil
 }
 
 func (b *s3Batch) Commit() error {
+	var (
+		deleteObjs []*s3.ObjectIdentifier
+		putKeys    []ds.Key
+	)
+	for k, op := range b.ops {
+		if op.delete {
+			deleteObjs = append(deleteObjs, &s3.ObjectIdentifier{
+				Key: aws.String(k),
+			})
+		} else {
+			putKeys = append(putKeys, ds.NewKey(k))
+		}
+	}
+
+	errChanSize := len(putKeys)
+	if len(deleteObjs) > 0 {
+		errChanSize++
+	}
+	errChan := make(chan error, errChanSize)
+	for _, k := range putKeys {
+		go func(k ds.Key, op batchOp) {
+			b.s.limiter <- struct{}{}
+
+			err := b.s.Put(k, op.val)
+			errChan <- err
+
+			<-b.s.limiter
+		}(k, b.ops[k.String()])
+	}
+
+	if len(deleteObjs) > 0 {
+		go func() {
+			resp, err := b.s.S3.DeleteObjects(&s3.DeleteObjectsInput{
+				Bucket: aws.String(b.s.Bucket),
+				Delete: &s3.Delete{
+					Objects: deleteObjs,
+				},
+			})
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			var errs []string
+			for _, err := range resp.Errors {
+				errs = append(errs, err.String())
+			}
+
+			if len(errs) > 0 {
+				err = fmt.Errorf("s3ds: failed to delete objects:\n%s", strings.Join(errs, "\n"))
+			}
+			errChan <- err
+		}()
+	}
+
+	for err := range errChan {
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
